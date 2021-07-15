9469e85a227d3b6d9a6fdc3705d75eda5fba0a37
guywhataguy <33753158+guywhataguy@users.noreply.github.com>
2021-07-16 00:50:05 +0300

    reformatting image caption

--- convert_blogger_xml_to_jekyll.py
+++ convert_blogger_xml_to_jekyll.py
@@ -3,6 +3,8 @@ from convert_blogger_xml_to_md import \
 	convert_posts_to_md, g_converter_config, converter_logger, HAPPY_LOG
 import urllib
 import os
+import string
+import html
 
 def make_post_preamble(post_data):
 	preamble = "---\n"
@@ -18,6 +20,26 @@ def make_post_preamble(post_data):
 	return preamble
 
 
+def clean_image_captions(md):
+	"""
+		find mardown like
+			![](/assets/img/posts\ssh+file+transfer.jpg)it's not stupid if it works
+		and change it to
+			{% include image.html url="/assets/img/posts\ssh+file+transfer.jpg" description="it's not stupid if it works" alt="some alt text" %}
+	"""
+	new_md = []
+	for md_line in md.split("\n"):
+		if md_line.startswith("![") and ("](" in md_line) and (")" in md_line):
+			alt_text = html.escape(md_line[2:md_line.index("](")])
+			img_path = html.escape(md_line[md_line.index("](")+2:md_line.index(")")])
+			img_capt = html.escape(md_line[md_line.index(")")+1:])
+
+			new_md.append(f'{{% include image.html url="{img_path}" caption="{img_capt}" alt="{alt_text}" %}}')
+		else:
+			new_md.append(md_line)
+
+	return "\n".join(new_md)
+
 def save_md_file_jekyll_style(post_data):
 
 	preamble = make_post_preamble(post_data)
@@ -26,9 +48,12 @@ def save_md_file_jekyll_style(post_data):
 	save_path = os.path.join(g_converter_config["md_file_save_path"], md_file_name)
 	converter_logger.log(HAPPY_LOG,f"saving '...{md_file_name}'")
 
+	# change images w/caption to be in the format of
+	# {% include image.html url="/assets/img/posts\telnet+into+router.jpg" description="easy work getting a shell" %}
 
+	cleaned_md = clean_image_captions(post_data["md"])
 
-	open(save_path, "w", encoding="utf-8").write(preamble + post_data["md"])
+	open(save_path, "w", encoding="utf-8").write(preamble + cleaned_md)
 
 def main():
 	if len(sys.argv) != 2:
